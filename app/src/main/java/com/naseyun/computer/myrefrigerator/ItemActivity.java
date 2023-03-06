91a4c56df25ce6e487bfd9d28a0097f8e184cc22
yangjimyeong <jmyounger@naver.com>
2023-03-06 15:42:39 +0900

    36

--- app/src/main/java/com/naseyun/computer/myrefrigerator/ItemActivity.java
+++ app/src/main/java/com/naseyun/computer/myrefrigerator/ItemActivity.java
@@ -1,22 +1,27 @@
 package com.naseyun.computer.myrefrigerator;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.os.ParcelFileDescriptor;
+import android.provider.OpenableColumns;
+import android.util.Log;
+import org.apache.commons.io.IOUtils;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -28,8 +33,14 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -37,12 +48,15 @@ public class ItemActivity extends AppCompatActivity {
     private int year = 0;
     private int month = 0;
     private int day = 0;
-    Bitmap bitmap;
-    private ImageView imageView;
-    Uri uri;
+
+    Button button;
+    ImageView imageView;
+
+    private File photoFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
 
@@ -68,51 +82,294 @@ public class ItemActivity extends AppCompatActivity {
             }
         });
 
-//        imageView = (ImageView) findViewById(CameraActivity);
-        Button picBtn = findViewById(R.id.button2);
-        Button galBtn = findViewById(R.id.button3);
+        button = findViewById(R.id.button2);
+        imageView = findViewById(R.id.imageView3);
 
-        picBtn.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int permissionCheck = ContextCompat.checkSelfPermission(ItemActivity.this, Manifest.permission.CAMERA);
-                if (permissionCheck == PackageManager.PERMISSION_DENIED) {
-                    ActivityCompat.requestPermissions(ItemActivity.this, new String[]{Manifest.permission.CAMERA}, 0);
-                } else {
-                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    activityResultPicture.launch(intent);
-                }
+                camera();
             }
         });
+
+    }
+
+    private void camera(){
+        int permissionCheck = ContextCompat.checkSelfPermission(
+                ItemActivity.this, Manifest.permission.CAMERA);
+
+        if(permissionCheck != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(ItemActivity.this,
+                    new String[]{Manifest.permission.CAMERA} ,
+                    1000);
+            Toast.makeText(ItemActivity.this, "카메라 권한 필요합니다.",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if(i.resolveActivity(ItemActivity.this.getPackageManager()) != null ){
+
+                // 사진의 파일명을 만들기
+                String fileName = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                photoFile = getPhotoFile(fileName);
+
+                Uri fileProvider = FileProvider.getUriForFile(ItemActivity.this,
+                        "com.naseyun.computer.myrefrigerator.fileprovider", photoFile);
+                i.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+                startActivityForResult(i, 100);
+
+            } else{
+                Toast.makeText(ItemActivity.this, "이폰에는 카메라 앱이 없습니다.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private File getPhotoFile(String fileName) {
+        File storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        try{
+            return File.createTempFile(fileName, ".jpg", storageDirectory);
+        }catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 0) {
-            if (grantResults[0] == 0) {
-                Toast.makeText(this, "카메라 권한이 승인됨", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(this, "카메라 권한이 거절 되었습니다.카메라를 이용하려면 권한을 승낙하여야 합니다.", Toast.LENGTH_SHORT).show();
+        switch (requestCode) {
+            case 1000: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(ItemActivity.this, "권한 허가 되었음",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(ItemActivity.this, "아직 승인하지 않았음",
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
             }
+            case 500: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(ItemActivity.this, "권한 허가 되었음",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(ItemActivity.this, "아직 승인하지 않았음",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
         }
     }
 
-    ActivityResultLauncher<Intent> activityResultPicture = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
-                        Bundle extras = result.getData().getExtras();
-                        bitmap = (Bitmap) extras.get("data");
-                        Intent intent = new Intent(getApplicationContext(), CameraActivity.class);
-                        intent.putExtra("image", bitmap);
-                        startActivity(intent);
-                        imageView = (ImageView) findViewById(R.id.imageView3);
-                        imageView.setImageBitmap(bitmap);
-                        //CameraActivity.ImageView.setImageBitmap(bitmap);
-                    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == 100 && resultCode == RESULT_OK){
+
+            Bitmap photo = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+
+            ExifInterface exif = null;
+            try {
+                exif = new ExifInterface(photoFile.getAbsolutePath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_UNDEFINED);
+            photo = rotateBitmap(photo, orientation);
+
+            // 압축시킨다. 해상도 낮춰서
+            OutputStream os;
+            try {
+                os = new FileOutputStream(photoFile);
+                photo.compress(Bitmap.CompressFormat.JPEG, 50, os);
+                os.flush();
+                os.close();
+            } catch (Exception e) {
+                Log.e(getClass().getSimpleName(), "Error writing bitmap", e);
+            }
+
+            photo = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+
+            imageView.setImageBitmap(photo);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+
+            // 네트워크로 데이터 보낸다.
+
+
+
+        }else if(requestCode == 300 && resultCode == RESULT_OK && data != null &&
+                data.getData() != null){
+
+            Uri albumUri = data.getData( );
+            String fileName = getFileName( albumUri );
+            try {
+
+                ParcelFileDescriptor parcelFileDescriptor = getContentResolver( ).openFileDescriptor( albumUri, "r" );
+                if ( parcelFileDescriptor == null ) return;
+                FileInputStream inputStream = new FileInputStream( parcelFileDescriptor.getFileDescriptor( ) );
+                photoFile = new File( this.getCacheDir( ), fileName );
+                FileOutputStream outputStream = new FileOutputStream( photoFile );
+                IOUtils.copy( inputStream, outputStream );
+
+//                //임시파일 생성
+//                File file = createImgCacheFile( );
+//                String cacheFilePath = file.getAbsolutePath( );
+
+
+                // 압축시킨다. 해상도 낮춰서
+                Bitmap photo = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                OutputStream os;
+                try {
+                    os = new FileOutputStream(photoFile);
+                    photo.compress(Bitmap.CompressFormat.JPEG, 60, os);
+                    os.flush();
+                    os.close();
+                } catch (Exception e) {
+                    Log.e(getClass().getSimpleName(), "Error writing bitmap", e);
                 }
-            });
+
+                imageView.setImageBitmap(photo);
+                imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+
+//                imageView.setImageBitmap( getBitmapAlbum( imageView, albumUri ) );
+
+            } catch ( Exception e ) {
+                e.printStackTrace( );
+            }
+
+            // 네트워크로 보낸다.
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public static Bitmap rotateBitmap(Bitmap bitmap, int orientation) {
+
+        Matrix matrix = new Matrix();
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_NORMAL:
+                return bitmap;
+            case ExifInterface.ORIENTATION_FLIP_HORIZONTAL:
+                matrix.setScale(-1, 1);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                matrix.setRotate(180);
+                break;
+            case ExifInterface.ORIENTATION_FLIP_VERTICAL:
+                matrix.setRotate(180);
+                matrix.postScale(-1, 1);
+                break;
+            case ExifInterface.ORIENTATION_TRANSPOSE:
+                matrix.setRotate(90);
+                matrix.postScale(-1, 1);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                matrix.setRotate(90);
+                break;
+            case ExifInterface.ORIENTATION_TRANSVERSE:
+                matrix.setRotate(-90);
+                matrix.postScale(-1, 1);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                matrix.setRotate(-90);
+                break;
+            default:
+                return bitmap;
+        }
+        try {
+            Bitmap bmRotated = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+            bitmap.recycle();
+            return bmRotated;
+        }
+        catch (OutOfMemoryError e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String getFileName( Uri uri ) {
+        Cursor cursor = getContentResolver( ).query( uri, null, null, null, null );
+        try {
+            if ( cursor == null ) return null;
+            cursor.moveToFirst( );
+            @SuppressLint("Range") String fileName = cursor.getString( cursor.getColumnIndex( OpenableColumns.DISPLAY_NAME ) );
+            cursor.close( );
+            return fileName;
+
+        } catch ( Exception e ) {
+            e.printStackTrace( );
+            cursor.close( );
+            return null;
+        }
+    }
+
+    public Bitmap getBitmapAlbum( View targetView, Uri uri ) {
+        try {
+            ParcelFileDescriptor parcelFileDescriptor = getContentResolver( ).openFileDescriptor( uri, "r" );
+            if ( parcelFileDescriptor == null ) return null;
+            FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor( );
+            if ( fileDescriptor == null ) return null;
+
+            int targetW = targetView.getWidth( );
+            int targetH = targetView.getHeight( );
+
+            BitmapFactory.Options options = new BitmapFactory.Options( );
+            options.inJustDecodeBounds = true;
+
+            BitmapFactory.decodeFileDescriptor( fileDescriptor, null, options );
+
+            int photoW = options.outWidth;
+            int photoH = options.outHeight;
+
+            int scaleFactor = Math.min( photoW / targetW, photoH / targetH );
+            if ( scaleFactor >= 8 ) {
+                options.inSampleSize = 8;
+            } else if ( scaleFactor >= 4 ) {
+                options.inSampleSize = 4;
+            } else {
+                options.inSampleSize = 2;
+            }
+            options.inJustDecodeBounds = false;
+
+            Bitmap reSizeBit = BitmapFactory.decodeFileDescriptor( fileDescriptor, null, options );
+
+            ExifInterface exifInterface = null;
+            try {
+                if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.N ) {
+                    exifInterface = new ExifInterface( fileDescriptor );
+                }
+            } catch ( IOException e ) {
+                e.printStackTrace( );
+            }
+
+            int exifOrientation;
+            int exifDegree = 0;
+
+            //사진 회전값 구하기
+            if ( exifInterface != null ) {
+                exifOrientation = exifInterface.getAttributeInt( ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL );
+
+                if ( exifOrientation == ExifInterface.ORIENTATION_ROTATE_90 ) {
+                    exifDegree = 90;
+                } else if ( exifOrientation == ExifInterface.ORIENTATION_ROTATE_180 ) {
+                    exifDegree = 180;
+                } else if ( exifOrientation == ExifInterface.ORIENTATION_ROTATE_270 ) {
+                    exifDegree = 270;
+                }
+            }
+
+            parcelFileDescriptor.close( );
+            Matrix matrix = new Matrix( );
+            matrix.postRotate( exifDegree );
+
+            Bitmap reSizeExifBitmap = Bitmap.createBitmap( reSizeBit, 0, 0, reSizeBit.getWidth( ), reSizeBit.getHeight( ), matrix, true );
+            return reSizeExifBitmap;
+
+        } catch ( Exception e ) {
+            e.printStackTrace( );
+            return null;
+        }
+
+    }
 }
